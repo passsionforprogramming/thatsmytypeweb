9e0aedd8dd30da311f55cf3553a427f4aec6f931
Nick S <nickjsaulsberry@outlook.com>
2020-04-28 01:54:01 -0500

    added signup route and programmatic access

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { MatchComponent } from './match/match.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 const routes: Routes = [
   {path: "", component: WelcomeComponent},
   {path: "match", component: MatchComponent},
-  {path: "signIn", component: SignInComponent}
+  {path: "signIn", component: SignInComponent},
+  {path: "signUp", component: SignUpComponent}
 ];
 
 @NgModule({
