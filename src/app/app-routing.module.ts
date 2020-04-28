a14c2c02f55f1d1f95c9d8266556381f96a3e21c
Nick S <nickjsaulsberry@outlook.com>
2020-04-27 21:00:44 -0500

    updating web project and creating navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import { WelcomeComponent } from './welcome/welcome.component';
+import { MatchComponent } from './match/match.component';
+const routes: Routes = [
+  {path: "", component: WelcomeComponent},
+  {path: "match", component: MatchComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { MatchComponent } from './match/match.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    WelcomeComponent,
+    MatchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
